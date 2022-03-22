13562cb6f4e0bccca33c21fa36d1c1e1961a31e3
MaximoEspitia <max19espitia@gmail.com>
2022-03-21 22:25:56 -0500

    commit tree 9ab69fa9a74171544fe783a384803fce13565b2d

--- demo.js
+++ demo.js
@@ -14,7 +14,9 @@ const colorStates = {
 // we want to make clickable. Without JavaScript, clicking on these heart shapes
 // does nothing. Uncomment the code and refresh the demo page. 
 
-// const articleHearts = document.querySelectorAll(".like-glyph");
+const articleHearts = document.querySelectorAll(".like-glyph");
+console.log(articleHearts);
+
 
 function likeCallback(e) {
   const heart = e.target;
